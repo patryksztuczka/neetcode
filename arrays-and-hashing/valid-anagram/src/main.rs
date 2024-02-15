09448cb5f1a87ccb249150ecaaf3e42c28193af1
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2024-02-15 19:34:39 +0100

    Aded third solution to contains duplicate problem

--- arrays-and-hashing/valid-anagram/src/main.rs
+++ arrays-and-hashing/valid-anagram/src/main.rs
@@ -5,12 +5,8 @@
 // appears at least twice in the array, and return false 
 // if every element is distinct.
 
-use std::collections::HashSet;
-
 fn main() {}
 
-// Time complexity: O(n^2)
-// Memory complexity: O(1)
 fn contains_duplicate(nums: Vec<i8>) -> bool {
     let mut contains_duplicate = false;
 
@@ -25,8 +21,6 @@ fn contains_duplicate(nums: Vec<i8>) -> bool {
     contains_duplicate
 }
 
-// Time complexity: O(nlogn)
-// Memory complexity: O(1)
 fn better_contains_duplicate(mut nums: Vec<i8>) -> bool {
     let mut contains_duplicate = false;
 
@@ -41,26 +35,10 @@ fn better_contains_duplicate(mut nums: Vec<i8>) -> bool {
     contains_duplicate
 }
 
-// Time complexity: O(n)
-// Memory complexity: O(n)
-fn the_best_contains_duplicate(nums: Vec<i8>) -> bool {
-    let mut hashset: HashSet<i8> = HashSet::new();
-
-    for num in nums {
-        if hashset.contains(&num) {
-            return true;
-        }
-        hashset.insert(num);
-    }
-
-    false
-}
-
 #[cfg(test)]
 mod tests {
     use crate::contains_duplicate;
     use crate::better_contains_duplicate;
-    use crate::the_best_contains_duplicate;
 
     #[test]
     fn test_contains_duplicate() {
@@ -82,16 +60,5 @@ mod tests {
         assert_eq!(better_contains_duplicate(nums1), true);
         assert_eq!(better_contains_duplicate(nums2), false);
         assert_eq!(better_contains_duplicate(nums3), true);
-    }
-
-    #[test]
-    fn test_the_best_contains_duplicate() {
-        let nums1 = vec![1,2,3,1];
-        let nums2 = vec![1,2,3,4];
-        let nums3 = vec![1,1,1,3,3,4,3,2,4,2];
-
-        assert_eq!(the_best_contains_duplicate(nums1), true);
-        assert_eq!(the_best_contains_duplicate(nums2), false);
-        assert_eq!(the_best_contains_duplicate(nums3), true);
     }
 }
